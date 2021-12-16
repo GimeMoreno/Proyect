e21424def6a6080971e9a43cfcc86d7d79f5b76b
GimeMoreno <96149787+GimeMoreno@users.noreply.github.com>
2021-12-16 12:41:36 -0300

    Update practicas.java

--- Hello/src/ejercicio/practicas.java
+++ Hello/src/ejercicio/practicas.java
@@ -4,7 +4,8 @@ public class practicas {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola amorcito");
+		System.out.println("Hola amorcitooooooo");
+		
 	}
 
 }
